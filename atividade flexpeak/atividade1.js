d1e3f892826ddf00d776e4fc59563d24125cc8cf
KAUAN MATHEUS MALOSTE MARTINS <kauanmmmartins@gmail.com>
2025-09-14 13:53:43 -0400

    Create atividade1.js

---
+++ atividade flexpeak/atividade1.js
@@ -0,0 +1,7 @@
+//Escreva um programa que determine se um ano é bissexto ou não.
+const ano = Number(prompt("Digite um ano: "))
+if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {
+    console.log(`${ano} é um ano bissexto.`)
+}else {
+    console.log(`${ano} não é um ano bissexto.`)
+}

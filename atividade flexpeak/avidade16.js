ac4321a91cd35cffb9dea03d6cedca29bc5e7b10
KAUAN MATHEUS MALOSTE MARTINS <kauanmmmartins@gmail.com>
2025-09-14 14:23:00 -0400

    Update avidade16.js

--- atividade flexpeak/avidade16.js
+++ atividade flexpeak/avidade16.js
@@ -1,1 +1,8 @@
+//Faça um programa que ordene três números em ordem crescente.
+const num1 = Number(prompt("Digite o primeiro número: "))
+const num2 = Number(prompt("Digite o segundo número: "))
+const num3 = Number(prompt("Digite o terceiro número: "))
+let numeros = [num1, num2, num3]
+numeros.sort((a, b) => a - b)
+console.log(`Números em ordem crescente: ${numeros.join(", ")}`)
 

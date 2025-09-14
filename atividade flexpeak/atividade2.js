d3a7911240e5ff6ab41e010d67c4e83f19bed856
KAUAN MATHEUS MALOSTE MARTINS <kauanmmmartins@gmail.com>
2025-09-14 13:54:53 -0400

    Create atividade2.js

---
+++ atividade flexpeak/atividade2.js
@@ -0,0 +1,7 @@
+//Crie um programa que verifique se uma pessoa é maior de idade (idade >= 18).
+const idade = Number(prompt("Digite sua idade: "))
+if (idade >= 18) {
+    console.log("Você é maior de idade.")
+}else {
+    console.log("Você é menor de idade.")
+}

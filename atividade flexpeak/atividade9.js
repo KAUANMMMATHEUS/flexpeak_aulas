d58883e1a8b279dd1b7f3fd18b0ada225d6d581a
KAUAN MATHEUS MALOSTE MARTINS <kauanmmmartins@gmail.com>
2025-09-14 14:03:56 -0400

    Update atividade9.js

--- atividade flexpeak/atividade9.js
+++ atividade flexpeak/atividade9.js
@@ -8,14 +8,4 @@ if (ladoA === ladoB && ladoB === ladoC) {
     console.log("O triângulo é isósceles.")
 }else {
     console.log("O triângulo é escaleno.")
-}//Escreva um programa que determine se um triângulo é equilátero, isósceles ou escaleno, com base em seus lados.
-const ladoA = Number(prompt("Digite o comprimento do lado A: "))
-const ladoB = Number(prompt("Digite o comprimento do lado B: "))
-const ladoC = Number(prompt("Digite o comprimento do lado C: "))
-if (ladoA === ladoB && ladoB === ladoC) {
-    console.log("O triângulo é equilátero.")
-}else if (ladoA === ladoB || ladoA === ladoC || ladoB === ladoC) {
-    console.log("O triângulo é isósceles.")
-}else {
-    console.log("O triângulo é escaleno.")
 }
